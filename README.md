050f922fc942f0d92dd9c3d2e41214d612a7204d
Ruqaiya <ruqaiya.sattar345@gmail.com>
2022-09-13 11:04:07 +0500

    combinelatest testing

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ This project is a starting point for a Flutter application.
 
 - Simple BehaviorStream text demo
 - Simple Searching App
-- 
+- Learn Rx Operators with Example
+(a) combineLatest : It combines the result of two or more stream => the latest state of two/more UI components
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart_demoapp/views/home_page.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
-    const App(),
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: const HomePage(),
+    ),
   );
 }
 
-class App extends StatelessWidget {
-  const App({
-    Key? key,
-  }) : super(key: key);
+void testIt() async {
+  final stream1 = Stream.periodic(
+    const Duration(seconds: 1), (count) => 'Stream 1, count = $count',
+  );
 
+  final stream2 = Stream.periodic(
+    const Duration(seconds: 3), (count) => 'Stream 2, count = $count',
+  );
+
+  final combine = Rx.combineLatest2(
+    stream1,
+    stream2,
+        (one, two) => 'One is ($one), Two is ($two)',
+  );
+  await for(final value in combine) {
+    value.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
     );
   }
 }
